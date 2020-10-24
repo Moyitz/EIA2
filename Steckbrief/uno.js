971d46fd11e669571da6aa3c21485e07d37afe85
Moyitz <moritz.labza@web.de>
2020-10-24 15:20:19 +0200

    miniunokonz

---
+++ Steckbrief/uno.js
@@ -0,0 +1,10 @@
+let cardamount = [];
+Check = window.prompt('Wie viele Karten?', '');
+if (Check == '') {
+    alert('Keine Angabe');
+}
+else {
+    cardamount.push(Check);
+    console.log(nn[0]);
+}
+//# sourceMappingURL=uno.js.map
---
+++ Steckbrief/uno.ts
@@ -0,0 +1,12 @@
+let cardamount:number [] =[]
+
+Check = window.prompt('Wie viele Karten?', '');
+if (Check == '') {
+  alert('Keine Angabe');
+} else {
+cardamount.push(Check);
+console.log(nn[0]);
+
+
+
+}
